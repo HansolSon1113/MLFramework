957dff134d61759a48ee02cce97ca0cbfeeb466b
YEAHx4 <developerstarlight@gmail.com>
2025-11-24 23:34:04 +0900

    chore: comment out invlaidated codes

--- src/main/kotlin/hs/ml/model/Model.kt
+++ src/main/kotlin/hs/ml/model/Model.kt
@@ -3,8 +3,8 @@ package hs.ml.model
 import hs.ml.math.Tensor
 
 abstract class Model {
-    protected abstract var weights: Tensor
-    protected abstract var bias: Double
+    abstract var weights: Tensor
+    abstract var bias: Double
     var param: ModelParameter = ModelParameter()
     var isTrained: Boolean = false
 
