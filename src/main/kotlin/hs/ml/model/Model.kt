ce2e7d3f69d262b05ec6350e75902e0dea257ac5
YEAHx4 <developerstarlight@gmail.com>
2025-11-24 22:48:09 +0900

    feat: regressor, classifier superclass

--- src/main/kotlin/hs/ml/model/Model.kt
+++ src/main/kotlin/hs/ml/model/Model.kt
@@ -7,4 +7,6 @@ abstract class Model {
     protected abstract var bias: Double
     protected abstract var isTrained: Boolean
     protected abstract var param: ModelParameter
+
+    abstract fun forward(x: Tensor): Tensor
 }
