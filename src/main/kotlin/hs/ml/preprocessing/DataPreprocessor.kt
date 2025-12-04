96397dcdb8f5fd842004330edd799b7e996a2309
YEAHx4 <developerstarlight@gmail.com>
2025-12-04 13:59:20 +0900

    feat: data preprocessing ui

--- src/main/kotlin/hs/ml/preprocessing/DataPreprocessor.kt
+++ src/main/kotlin/hs/ml/preprocessing/DataPreprocessor.kt
@@ -6,11 +6,18 @@ import hs.ml.scaler.Scaler
 
 class DataPreprocessor(
     private val missingPolicy: MissingPolicy,
-    private val scaler: Scaler
+    private val scaler: Scaler?
 ){
     fun process(batch: DataBatch):DataBatch{
         println("DataPreprocessor: 전처리 시작...")
         val processedInputs = missingPolicy.handle(batch.inputs)
+
+        if (scaler == null) {
+            return DataBatch(
+                inputs = processedInputs,
+                labels = batch.labels
+            )
+        }
 
         scaler.fit(processedInputs)
         val scaledInputs = scaler.transform(processedInputs)
--- src/main/kotlin/hs/ml/ui/ConsoleUI.kt
+++ src/main/kotlin/hs/ml/ui/ConsoleUI.kt
@@ -1,8 +1,17 @@
 package hs.ml.ui
 
 import hs.ml.data.DataBatch
+import hs.ml.data.DataPipeline
 import hs.ml.importer.CsvImporter
+import hs.ml.importer.DataImporter
 import hs.ml.math.Tensor
+import hs.ml.preprocessing.DataPreprocessor
+import hs.ml.preprocessing.policy.MissingPolicy
+import hs.ml.preprocessing.policy.ReplaceToAvgPolicy
+import hs.ml.preprocessing.policy.ReplaceToZeroPolicy
+import hs.ml.scaler.MinMaxScaler
+import hs.ml.scaler.Scaler
+import hs.ml.scaler.StandardScaler
 import java.io.File
 import java.util.Scanner
 
@@ -11,7 +20,26 @@ class ConsoleUI {
 
     private var data = DataBatch(Tensor(), Tensor())
 
-    fun getData(): DataBatch {
+    private fun select(options: List<String>): Int {
+        for ((index, option) in options.withIndex()) {
+            println("${index + 1}. $option")
+        }
+
+        var choice: Int
+        do {
+            print("> ")
+            val input = scanner.nextLine()
+            choice = input.toIntOrNull() ?: -1
+            if (choice !in 1..options.size) {
+                println("유효한 선택지가 아닙니다. 다시 시도해주세요.")
+            } else {
+                break
+            }
+        } while (true)
+        return choice
+    }
+
+    fun getImporter(): DataImporter {
         var filePath: String
         do {
             print("CSV 파일 경로 : ")
@@ -25,13 +53,43 @@ class ConsoleUI {
             }
         } while (true)
 
-        return CsvImporter(filePath).read()
+        return CsvImporter(filePath)
+    }
+
+    fun getScaler(): Scaler? {
+        println("스케일러 선택:")
+        val s = select(listOf("StandardScaler", "MinMaxScaler", "None"))
+        return when (s) {
+            1 -> StandardScaler()
+            2 -> MinMaxScaler()
+            else -> null
+        }
+    }
+
+    fun getMissingPolicy(): MissingPolicy {
+        println("결측치 처리 정책 선택:")
+        val p = select(listOf("평균값으로 대체", "0으로 대체"))
+
+        return when (p) {
+            1 -> ReplaceToAvgPolicy()
+            else -> ReplaceToZeroPolicy()
+        }
     }
 
     fun start() {
         println("OOP Machine Learning Project")
 
         println("\n**데이터 전처리 단계**")
-        data = getData()
+        val importer = getImporter()
+        val scaler = getScaler()
+        val policy = getMissingPolicy()
+
+        val pipeline = DataPipeline(
+            importer = importer,
+            preprocessor = DataPreprocessor(policy, scaler)
+        )
+
+        data = pipeline.run()
+        println("데이터 전처리 완료. 데이터 크기: ${data.inputs.shape}")
     }
 }
