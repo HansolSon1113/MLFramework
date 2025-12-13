c26369558536796abedf087594d0a55d431f0a26
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2025-12-13 21:07:59 +0900

    fix: no negative input & output size

--- src/main/kotlin/hs/ml/model/nn/Dense.kt
+++ src/main/kotlin/hs/ml/model/nn/Dense.kt
@@ -5,12 +5,25 @@ import hs.ml.autograd.Node
 import hs.ml.math.Tensor
 import kotlin.math.sqrt
 
-class Dense(val inputSize: Int, val outputSize: Int) : Layer() {
-    var weights = Node(Tensor(inputSize, outputSize) { _, _ ->
-        ThreadLocalRandom.current().nextGaussian() * sqrt(2.0 / inputSize)
-    })
+class Dense : Layer {
+    val inputSize: Int
+    val outputSize: Int
+
+    constructor(inputSize: Int, outputSize: Int) : super() {
+        require(inputSize > 0)
+        require(outputSize > 0)
+
+        this.inputSize = inputSize
+        this.outputSize = outputSize
+        this.weights = Node(Tensor(inputSize, outputSize) { _, _ ->
+            ThreadLocalRandom.current().nextGaussian() * sqrt(2.0 / inputSize)
+        })
+        this.bias = Node(Tensor(1, outputSize, 0.0))
+    }
+
+    var weights: Node
         private set
-    var bias = Node(Tensor(1, outputSize, 0.0))
+    var bias: Node
         private set
 
     override fun forward(input: Node): Node {
